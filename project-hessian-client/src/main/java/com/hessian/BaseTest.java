2da50f77c121fbb357730ac7248299a87c9f1160
18611478781 <18611478781@163.com>
2016-04-24 11:05:26 +0800

    基础测试

--- project-hessian-client/src/main/java/com/hessian/BaseTest.java
+++ project-hessian-client/src/main/java/com/hessian/BaseTest.java
@@ -4,6 +4,13 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+/**
+ * 基础测试环境
+ * 
+ * @author Liubao
+ * @2016年4月24日
+ *
+ */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { 
         "classpath:remote-hessian.xml"
